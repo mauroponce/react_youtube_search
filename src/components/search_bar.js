e404bcd69bace9b3e2a24c355cc8fc1e03958948
Mauro Ponce <mgp1989@gmail.com>
2017-01-14 02:44:38 -0300

    Add search button and some style

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -6,21 +6,33 @@ class SearchBar extends Component {
     this.state = { term: '' };
   }
 
-  handleKeyPress(event){
+  handleKeyPress(event) {
+    let term = event.target.value;
     if(event.key == 'Enter') {
-      let term = event.target.value;
+      this.performSearch();
+    } else {
       this.setState({ term: term });
-      this.props.onSearchTermChange(term);
     }
+  }
+
+  performSearch() {
+    this.props.onSearchTermChange(this.state.term);
   }
 
   render() {
     return (
-      <div className="search-bar">
+      <div className="search-bar input-group">
         <input
           placeholder="Search"
-          onKeyPress={event => this.handleKeyPress(event)}
+          onKeyUp={event => this.handleKeyPress(event)}
           className="form-control" />
+        <div className="input-group-btn">
+          <button
+            onClick={() => this.performSearch()}
+            className="btn btn-default">
+            <i className="fa fa-search"></i>
+          </button>
+        </div>
       </div>
     );
   }
